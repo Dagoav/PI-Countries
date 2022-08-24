9cc2d807bb936590a702ed384a098211411a4519
dagoav <dagoav89@gmail.com>
2022-08-24 16:17:04 -0500

    fix DB_NAME, package.json

--- api/src/db.js
+++ api/src/db.js
@@ -31,7 +31,7 @@ if (process.env.NODE_ENV === "production") {
     ssl: true
   })
 } else {
-  sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/countries`, {
+  sequelize = new Sequelize(`postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`, {
     logging: false, // set to console.log to see the raw SQL queries
     native: false, // lets Sequelize know we can use pg-native for ~30% more speed
   });
